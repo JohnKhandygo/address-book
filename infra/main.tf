77563ea739b0aa4ec7820aab7b5a2109b703dfd8
Evgenii Khandygo <Evgenii_Khandygo@epam.com>
2023-08-02 15:09:08 +0200

    infra: add VPC (with subnets, IG, NAT, etc.).

--- infra/main.tf
+++ infra/main.tf
@@ -9,3 +9,24 @@ terraform {
 
 provider "aws" {
 }
+
+data "aws_region" "current" {}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~>5.0"
+
+  name = "vpc-02"
+  cidr = "10.0.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+  intra_subnets   = ["10.0.201.0/24", "10.0.202.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+}
